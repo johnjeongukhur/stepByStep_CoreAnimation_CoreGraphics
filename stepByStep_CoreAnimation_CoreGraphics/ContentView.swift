fab96368ab53e4d9e3f8c57e5ddda1224ad83b9d
johnjeongukhur <johnjeongukhur@gmail.com>
2022-08-03 22:47:14 +0900

    Initial Commit

---
+++ stepByStep_CoreAnimation_CoreGraphics/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  stepByStep_CoreAnimation_CoreGraphics
+//
+//  Created by John Hur on 2022/08/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ stepByStep_CoreAnimation_CoreGraphics/stepByStep_CoreAnimation_CoreGraphicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  stepByStep_CoreAnimation_CoreGraphicsApp.swift
+//  stepByStep_CoreAnimation_CoreGraphics
+//
+//  Created by John Hur on 2022/08/03.
+//
+
+import SwiftUI
+
+@main
+struct stepByStep_CoreAnimation_CoreGraphicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
