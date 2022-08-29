db2bf6ba12f1882ee80af520c4acab9e1a56a8e6
johnjeongukhur <johnjeongukhur@gmail.com>
2022-08-29 22:11:24 +0900

    add/feat-change_postition

--- stepByStep_CoreAnimation_CoreGraphics/animation_4.swift
+++ stepByStep_CoreAnimation_CoreGraphics/animation_4.swift
@@ -11,17 +11,22 @@ struct animation_4: View {
     @State var rotation = 0.0
     @State var scaleXY = 1.0
     
+    // Initial position
+    @State var positionX = -150
+    
     var body: some View {
-        HStack {
+        VStack(alignment: .center) {
             Rectangle()
                 .fill(.purple)
                 .frame(width: 50, height: 50)
                 .rotationEffect(.degrees(rotation))
                 .scaleEffect(CGFloat(scaleXY))
+                .offset(x: CGFloat(positionX))
                 .animation(Animation.easeInOut(duration: 3).repeatForever(autoreverses: true).speed(4))
                 .onAppear() {
                     rotation += 360
                     scaleXY += 1.1
+                    positionX += 250
                 }
         }
     }
